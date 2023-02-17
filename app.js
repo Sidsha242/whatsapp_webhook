de237fd3e49a6f17b0c734a6a928ed886e383da2
Glitch (messenger-platform-quick-start) <none>
2023-02-17 10:01:16 +0000

    ⛱📌 Checkpoint ./app.js:54738264/6

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post("/webhook", (req, res) => {
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
       console.log(msg_body);
-      console.log("From:")
+      console.log("From:"+from);
       axios({
         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
         url:
